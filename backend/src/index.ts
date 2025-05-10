83cbf1b6b255f451131aa2ccc53911217f292e88
Vivek kumar <vivek.buisness@gmail.com>
2025-05-10 10:01:50 +0530

    added type - module

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,18 @@
 import express from "express";
-import cookieParser from "cookie-parser";
-import path from "path";
-
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
-const __dirname = path.resolve();
 
 const app = express();
 
-app.use(cookieParser()); // for parsing cookies
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 
-if (process.env.NODE_ENV !== "development") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
+if (!process.env.PORT) {
+  console.warn("PORT not defined in .env. Defaulting to 8080.");
 }
 
 app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+  console.log(`Server is running on port ${PORT}`);
 });
